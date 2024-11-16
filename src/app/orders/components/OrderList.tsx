d7cdd900a88d3d70c698a1a31f0f9a9d2b2622d9
alfonsod1986 <alfonsod18@gmail.com>
2024-11-16 13:51:34 -0600

    chore: Add OrderList and OrderListSkeleton components

---
+++ src/app/orders/components/OrderList.tsx
@@ -0,0 +1,68 @@
+import Link from "next/link";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { getAllOrders, IOrder } from "@/actions/order";
+
+const OrderList = async () => {
+  const orders: IOrder[] = await getAllOrders();
+
+  return (
+    <Table>
+      <TableCaption>A list of your recent orders</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Order</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead className="hidden md:table-cell">Date</TableHead>
+          <TableHead>Taxes</TableHead>
+          <TableHead>Discount</TableHead>
+          <TableHead>Subtotal</TableHead>
+          <TableHead>Total</TableHead>
+          <TableHead className="text-right">Actions</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {orders.map((order) => (
+          <TableRow key={order.id}>
+            <TableCell className="font-medium">{order.id}</TableCell>
+            <TableCell>
+              <span
+                className={`px-2 py-1 rounded-full text-xs font-semibold ${
+                  order.paid
+                    ? "bg-green-100 text-green-800"
+                    : "bg-red-100 text-red-800"
+                }`}
+              >
+                {order.paid ? "Paid" : "Unpaid"}
+              </span>
+            </TableCell>
+            <TableCell className="hidden md:table-cell">
+              {order.created.split("T")[0]}
+            </TableCell>
+            <TableCell>${order.taxes}</TableCell>
+            <TableCell>${order.discount}</TableCell>
+            <TableCell>${order.subtotal}</TableCell>
+            <TableCell>${order.total}</TableCell>
+            <TableCell className="text-right">
+              <Link href={`/orders/${order.id}`} passHref>
+                <Button variant="outline" size="sm">
+                  View Details
+                </Button>
+              </Link>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default OrderList;
